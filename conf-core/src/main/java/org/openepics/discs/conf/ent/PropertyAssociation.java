2bc7adaf14db52e420c56a1a720950da8ad31c7f
Miha Vitorovic <miha.vitorovic@cosylab.com>
2014-07-08 13:16:25 +0200

    Added developer warning comment to PropertyAssociation.java

--- conf-core/src/main/java/org/openepics/discs/conf/ent/PropertyAssociation.java
+++ conf-core/src/main/java/org/openepics/discs/conf/ent/PropertyAssociation.java
@@ -1,5 +1,19 @@
 package org.openepics.discs.conf.ent;
 
+
+/* ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! *
+ * WARNING! PLEASE NOTE! WARNING! PLEASE NOTE! WARNING! PLEASE NOTE! WARNING! PLEASE NOTE! WARNING! PLEASE NOTE!
+ *
+ * The field in the Property.java file persisting this entity is limited to 12 characters.
+ *
+ * @Enumerated(EnumType.STRING)
+ * @Column(name = "association", length = 12)
+ *
+ * If you add another enumeration property that goes over this limit, remember to update the database model accordingly.
+ *
+ * WARNING! PLEASE NOTE! WARNING! PLEASE NOTE! WARNING! PLEASE NOTE! WARNING! PLEASE NOTE! WARNING! PLEASE NOTE!
+ * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! * ! */
+
 public enum PropertyAssociation {
 	/** The property is only for <i>Component Type</i>, E.g.: a CAD drawing. */
 	TYPE,
