1775fd4490f727298156ffb50160f49c9499c990
Miha Vitorovic <miha.vitorovic@cosylab.com>
2014-07-11 11:48:33 +0200

    Reduced chattiness of the AuthEJB.

--- conf-core/src/main/java/org/openepics/discs/conf/ejb/AuthEJB.java
+++ conf-core/src/main/java/org/openepics/discs/conf/ejb/AuthEJB.java
@@ -28,7 +28,7 @@ public class AuthEJB {
     @PersistenceContext(unitName = "org.openepics.discs.conf.data")
     private EntityManager em;
 
-    // todo: implement autorization using RBAC.
+    // TODO: implement authorization using RBAC.
     public boolean userHasAuth(String principal, EntityType resource, EntityTypeOperation operation) {
         boolean auth = false;
 
@@ -43,11 +43,11 @@ public class AuthEJB {
                 .setParameter("user", principal);
 
         privs = query.getResultList();
-        logger.info("AuthEJB: found privileges: " + privs.size());
+        logger.finer("found privileges: " + privs.size());
 
         for (Privilege p : privs) {
             if (resource.equals(p.getResource())) {
-                logger.info("AuthEJB: matched privileges: " + p);
+                logger.finest("matched privileges: " + p);
                 if (p.getOper().equals(operation)) {
                     auth = true;
                     break;
