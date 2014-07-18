0b6b29980e37287b079d49c1c818a7e0ea86ec77
Andraz Pozar <andraz.pozar@cosylab.com>
2014-07-18 13:32:20 +0200

    Added tests for units data loader. Tests do not read or write from the database yet.

--- conf-core/src/test/java/org/openepics/discs/conf/dl/UnitsDataLoaderIT.java
+++ conf-core/src/test/java/org/openepics/discs/conf/dl/UnitsDataLoaderIT.java
@@ -7,16 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
-
-import org.fest.assertions.AssertExtension;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openepics.discs.conf.dl.common.DataLoader;
 import org.openepics.discs.conf.dl.common.DataLoaderResult;
 import org.openepics.discs.conf.dl.common.DataLoaderResult.RowFormatFailureDataLoaderResult;
+import org.openepics.discs.conf.ejb.ConfigurationEJB;
+import org.openepics.discs.conf.ui.LoginManager;
 
 import com.google.common.collect.ImmutableList;
 
@@ -30,20 +31,55 @@ import com.google.common.collect.ImmutableList;
 public class UnitsDataLoaderIT {
 
     @Inject @UnitLoaderQualifier private DataLoader unitsDataLoader;
+    @Inject private ConfigurationEJB confEJB;
+    @Inject private LoginManager loginManager;
+    private List<List<String>> inputRows;
 
     @Deployment
     public static WebArchive createDeployment() {
         return CCDBPackager.createWebArchive();
     }
 
-    @Test
-    public void importUnitsHeaderTest() {
-        final List<List<String>> imputRows = new ArrayList<>();
-        imputRows.add(ImmutableList.of("HEADER"));
+    @Before
+    public void setUpBeforeTest() {
+        inputRows = new ArrayList<>();
+    }
 
-        final DataLoaderResult dataLoaderResult = unitsDataLoader.loadDataToDatabase(imputRows);
+    @Test
+    public void notAuthorizedTest() {
+        inputRows.add(ImmutableList.of("1", "HEADER", "NAME", "SYMBOL", "EXPR", "QUANTITY", "DESCRIPTION"));
+        inputRows.add(ImmutableList.of("2", "UPDATE", "Ampere", "A", "", "Current", "Electric current"));
+        DataLoaderResult dataLoaderResult = unitsDataLoader.loadDataToDatabase(inputRows);
+        assertThat(dataLoaderResult, instanceOf(DataLoaderResult.NotAuthorizedFailureDataLoaderResult.class));
+
+        inputRows.remove(1);
+        inputRows.add(ImmutableList.of("2", "DELETE", "Ampere", "A", "", "Current", "Electric current"));
+        dataLoaderResult = unitsDataLoader.loadDataToDatabase(inputRows);
+        assertThat(dataLoaderResult, instanceOf(DataLoaderResult.NotAuthorizedFailureDataLoaderResult.class));
+
+        inputRows.remove(1);
+        inputRows.add(ImmutableList.of("2", "RENAME", "Ampere", "A", "", "Current", "Electric current"));
+        dataLoaderResult = unitsDataLoader.loadDataToDatabase(inputRows);
+        assertThat(dataLoaderResult, instanceOf(DataLoaderResult.NotAuthorizedFailureDataLoaderResult.class));
+    }
+
+    /**
+     * Test if data loader correctly detects missing header fields and returns correct {@link DataLoaderResult}
+     */
+    @Test
+    public void headerMissingFieldsTest() {
+        inputRows.add(ImmutableList.of("HEADER"));
+
+        DataLoaderResult dataLoaderResult = unitsDataLoader.loadDataToDatabase(inputRows);
+        assertThat(dataLoaderResult, instanceOf(DataLoaderResult.RowFormatFailureDataLoaderResult.class));
+        assertEquals(((RowFormatFailureDataLoaderResult)dataLoaderResult).getReason(), DataLoaderResult.RowFormatFailureReason.HEADER_FIELD_MISSING);
+
+        inputRows.clear();
+
+        inputRows.add(ImmutableList.of("HEADER", "NAME", "SYMBOL", "EXPRESSION", "QUANTITY", "DESCRIPTION"));
+
+        dataLoaderResult = unitsDataLoader.loadDataToDatabase(inputRows);
         assertThat(dataLoaderResult, instanceOf(DataLoaderResult.RowFormatFailureDataLoaderResult.class));
         assertEquals(((RowFormatFailureDataLoaderResult)dataLoaderResult).getReason(), DataLoaderResult.RowFormatFailureReason.HEADER_FIELD_MISSING);
     }
-
 }
