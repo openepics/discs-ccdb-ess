23bcd9e7d1e6b2596bc1b89e6692d330335d5f8a
Miha Vitorovic <miha.vitorovic@cosylab.com>
2014-08-06 08:27:33 +0200

    Added table for saving reports.

---
+++ conf-core/src/main/java/org/openepics/discs/conf/ent/Report.java
@@ -0,0 +1,70 @@
+package org.openepics.discs.conf.ent;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.openepics.discs.conf.util.As;
+
+/**
+ *
+ * @author Miha Vitorovič <miha.vitorovic@cosylab.com>
+ *
+ */
+@Entity
+@Table(name = "report")
+public class Report extends ConfigurationEntity {
+    private static final long serialVersionUID = 1L;
+
+    @Basic(optional = false)
+    @Nonnull
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "filter_by_types", joinColumns = { @JoinColumn(name = "report_id", referencedColumnName = "id") }, inverseJoinColumns = { @JoinColumn(name = "type_id", referencedColumnName = "id") })
+    private List<ComponentType> typeFilters;
+
+    @OneToMany(mappedBy = "parentReport")
+    private List<ReportAction> filters;
+
+    protected Report() {
+    }
+
+    public Report(String name, String modifiedBy) {
+        this.name = As.notNull(name);
+        this.typeFilters = new ArrayList<>();
+        this.filters = new ArrayList<>();
+        this.modifiedAt = new Date();
+        this.modifiedBy = modifiedBy;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = As.notNull(name); }
+
+    public List<ComponentType> getTypeFilters() { return typeFilters; }
+    public void setTypeFilters(List<ComponentType> typeFilters) {
+        if (typeFilters == null)
+            this.typeFilters.clear();
+        else
+            this.typeFilters = typeFilters;
+    }
+
+    public List<ReportAction> getFilters() { return filters; }
+    public void setFilters(List<ReportAction> filters) {
+        for (ReportAction filter : As.notNull(filters))
+            if (filter.getParentReport() != this)
+                throw new IllegalArgumentException("Filter does not belong to parent. " + filter.toString());
+        this.filters = filters;
+    }
+}
---
+++ conf-core/src/main/java/org/openepics/discs/conf/ent/ReportAction.java
@@ -0,0 +1,131 @@
+package org.openepics.discs.conf.ent;
+
+import java.util.Date;
+
+import javax.annotation.Nonnull;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.openepics.discs.conf.util.As;
+
+/**
+ *
+ * @author Miha Vitorovič <miha.vitorovic@cosylab.com>
+ *
+ */
+@Entity
+@Table(name = "report_action")
+public class ReportAction extends ConfigurationEntity {
+    private static final long serialVersionUID = 1L;
+
+    @Basic(optional = false)
+    @Nonnull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "operation")
+    private ReportFilterAction operation;
+
+    @Basic
+    @Column(name = "field")
+    private String field;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @Basic
+    @Column(name = "value")
+    private String value;
+
+    @Nonnull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "report_id")
+    private Report parentReport;
+
+    protected ReportAction() {
+    }
+
+    public ReportAction(ReportFilterAction operation, String field, String value, Report parentReport, String modifiedBy) {
+        this.operation = As.notNull(operation);
+        this.field = As.notNull(field);
+        this.value = As.notNull(value);
+        this.parentReport = As.notNull(parentReport);
+        this.modifiedBy = modifiedBy;
+        this.modifiedAt = new Date();
+    }
+
+    public ReportAction(ReportFilterAction operation, Property property, String value, Report parentReport, String modifiedBy) {
+        this.operation = As.notNull(operation);
+        this.property = As.notNull(property);
+        this.value = As.notNull(value);
+        this.parentReport = As.notNull(parentReport);
+        this.modifiedBy = modifiedBy;
+        this.modifiedAt = new Date();
+    }
+
+    public ReportAction(ReportFilterAction operation, Tag tag, Report parentReport, String modifiedBy) {
+        if (operation != ReportFilterAction.IS)
+            throw new IllegalArgumentException("Tag can only have an \"IS\" operation.");
+        this.operation = operation;
+        this.tag = As.notNull(tag);
+        this.parentReport = As.notNull(parentReport);
+        this.modifiedBy = modifiedBy;
+        this.modifiedAt = new Date();
+    }
+
+    public ReportFilterAction getOperation() { return operation; }
+    public void setOperation(ReportFilterAction operation) {
+        if (tag != null && operation != ReportFilterAction.IS)
+            throw new IllegalArgumentException("Tag can only have an \"IS\" operation.");
+        this.operation = operation;
+    }
+
+    public String getField() { return field; }
+    public void setField(String field) {
+        this.field = field;
+        this.property = null;
+        this.tag = null;
+    }
+
+    public Property getProperty() { return property; }
+    public void setProperty(Property property) {
+        this.property = property;
+        this.field = null;
+        this.tag = null;
+    }
+
+    public Tag getTag() { return tag; }
+    public void setTag(Tag tag) {
+        this.tag = tag;
+        this.field = null;
+        this.property = null;
+    }
+
+    public String getValue() { return value; }
+    public void setValue(String value) {
+        if (tag != null)
+            throw new IllegalStateException("Cannot set a value on a filter by tag.");
+        this.value = value;
+    }
+
+    public Report getParentReport() { return parentReport; }
+    public void setParentReport(Report parentReport) { this.parentReport = parentReport; }
+
+    @Override
+    public String toString() {
+        if (field != null) return "[ Field: " + field + " " + operation.toString() + " " + value + " ]";
+        if (property != null) return "[ Property: " + property.getName() + " " + operation.toString() + " " + value + " ]";
+        if (tag != null) return "[ Tag IS " + tag.getName() + " ]";
+        return "Error! Invalid report filter definition.";
+    }
+}
