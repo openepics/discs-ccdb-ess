3b756fa15ff5be43333098fdac33c4da9e5f9186
mpavleski <miroslav.pavleski@cosylab.com>
2014-08-19 23:55:46 +0200

    CCDB-73 Fix - EntityTypeResolver uses type-safe mappings (will survive refactor)

--- conf-core/src/main/java/org/openepics/discs/conf/security/EntityTypeResolver.java
+++ conf-core/src/main/java/org/openepics/discs/conf/security/EntityTypeResolver.java
@@ -5,39 +5,45 @@ import java.util.Map;
 
 import org.openepics.discs.conf.ent.EntityType;
 
+/**
+ * 
+ * Helper class used to resolve the {@link EntityType} of configuration entities, relevant to audit logging and security
+ * 
+ * @author Miroslav Pavleski <miroslav.pavleski@cosylab.com>
+ *
+ */
 public class EntityTypeResolver {
     static Map<String, EntityType> entityTypes = new HashMap<> ();
     
     static {
-        entityTypes.put("org.openepics.discs.conf.ent.Device", EntityType.DEVICE);
-        entityTypes.put("org.openepics.discs.conf.ent.DevicePropertyValue", EntityType.DEVICE);
-        entityTypes.put("org.openepics.discs.conf.ent.DeviceArtifact", EntityType.DEVICE);
+        entityTypes.put(org.openepics.discs.conf.ent.Device.class.getCanonicalName(), EntityType.DEVICE);
+        entityTypes.put(org.openepics.discs.conf.ent.DevicePropertyValue.class.getCanonicalName(), EntityType.DEVICE);
+        entityTypes.put(org.openepics.discs.conf.ent.DeviceArtifact.class.getCanonicalName(), EntityType.DEVICE);
                                
-        entityTypes.put("org.openepics.discs.conf.ent.Slot", EntityType.SLOT);
-        entityTypes.put("org.openepics.discs.conf.ent.SlotPropertyValue", EntityType.SLOT);
-        entityTypes.put("org.openepics.discs.conf.ent.SlotArtifact", EntityType.SLOT);
-        entityTypes.put("org.openepics.discs.conf.ent.SlotPair", EntityType.SLOT);       
+        entityTypes.put(org.openepics.discs.conf.ent.Slot.class.getCanonicalName(), EntityType.SLOT);
+        entityTypes.put(org.openepics.discs.conf.ent.SlotPropertyValue.class.getCanonicalName(), EntityType.SLOT);
+        entityTypes.put(org.openepics.discs.conf.ent.SlotArtifact.class.getCanonicalName(), EntityType.SLOT);
+        entityTypes.put(org.openepics.discs.conf.ent.SlotPair.class.getCanonicalName(), EntityType.SLOT);       
         
-        entityTypes.put("org.openepics.discs.conf.ent.ComponentType", EntityType.COMPONENT_TYPE);
-        entityTypes.put("org.openepics.discs.conf.ent.ComptypePropertyValue", EntityType.COMPONENT_TYPE);
-        entityTypes.put("org.openepics.discs.conf.ent.ComptypeArtifact", EntityType.COMPONENT_TYPE);
-        entityTypes.put("org.openepics.discs.conf.ent.ComptypeAsm", EntityType.COMPONENT_TYPE);
+        entityTypes.put(org.openepics.discs.conf.ent.ComponentType.class.getCanonicalName(), EntityType.COMPONENT_TYPE);
+        entityTypes.put(org.openepics.discs.conf.ent.ComptypePropertyValue.class.getCanonicalName(), EntityType.COMPONENT_TYPE);
+        entityTypes.put(org.openepics.discs.conf.ent.ComptypeArtifact.class.getCanonicalName(), EntityType.COMPONENT_TYPE);
+        entityTypes.put(org.openepics.discs.conf.ent.ComptypeAsm.class.getCanonicalName(), EntityType.COMPONENT_TYPE);
                        
         
-        entityTypes.put("org.openepics.discs.conf.ent.InstallationRecord", EntityType.INSTALLATION_RECORD);
-        entityTypes.put("org.openepics.discs.conf.ent.InstallationArtifact", EntityType.INSTALLATION_RECORD);
+        entityTypes.put(org.openepics.discs.conf.ent.InstallationRecord.class.getCanonicalName(), EntityType.INSTALLATION_RECORD);
+        entityTypes.put(org.openepics.discs.conf.ent.InstallationArtifact.class.getCanonicalName(), EntityType.INSTALLATION_RECORD);
         
-        entityTypes.put("org.openepics.discs.conf.ent.AlignmentRecord", EntityType.ALIGNMENT_RECORD);
-        entityTypes.put("org.openepics.discs.conf.ent.AlignmentArtifact", EntityType.ALIGNMENT_RECORD);
-        entityTypes.put("org.openepics.discs.conf.ent.AlignmentPropertyValue", EntityType.ALIGNMENT_RECORD);
+        entityTypes.put(org.openepics.discs.conf.ent.AlignmentRecord.class.getCanonicalName(), EntityType.ALIGNMENT_RECORD);
+        entityTypes.put(org.openepics.discs.conf.ent.AlignmentArtifact.class.getCanonicalName(), EntityType.ALIGNMENT_RECORD);
+        entityTypes.put(org.openepics.discs.conf.ent.AlignmentPropertyValue.class.getCanonicalName(), EntityType.ALIGNMENT_RECORD);
         
-        entityTypes.put("org.openepics.discs.conf.ent.Unit", EntityType.UNIT);
+        entityTypes.put(org.openepics.discs.conf.ent.Unit.class.getCanonicalName(), EntityType.UNIT);
         
-        entityTypes.put("org.openepics.discs.conf.ent.User", EntityType.USER);
-        entityTypes.put("org.openepics.discs.conf.ent.Property", EntityType.PROPERTY);
+        entityTypes.put(org.openepics.discs.conf.ent.User.class.getCanonicalName(), EntityType.USER);
+        entityTypes.put(org.openepics.discs.conf.ent.Property.class.getCanonicalName(), EntityType.PROPERTY);
        
-        entityTypes.put("org.openepics.discs.conf.ent.DataType", EntityType.DATA_TYPE);        
-    }
+        entityTypes.put(org.openepics.discs.conf.ent.DataType.class.getCanonicalName(), EntityType.DATA_TYPE);       }
     
     /**
      * Resolves the {@link EntityType} of an entity object, for security purposes.
