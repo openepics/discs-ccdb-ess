73245462bc2a2a867d74e536af1b35e20f1ab3d4
mpavleski <miroslav.pavleski@cosylab.com>
2014-08-26 16:58:44 +0200

    CCDB-65 Setting CompType Integration Tests infrastructure

---
+++ conf-core/src/test/java/org/openepics/discs/conf/ejb/AlignmentEJBIT.java
@@ -0,0 +1,76 @@
+package org.openepics.discs.conf.ejb;
+
+import static org.junit.Assert.*;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.openepics.discs.conf.util.CCDBPackager;
+
+public class AlignmentEJBIT {
+//    @Deployment
+//    public static WebArchive createDeployment() {
+//        return CCDBPackager.createWebArchive();
+//    }    
+//    
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @Test
+//    public void testFindAlignmentRec() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testFindAlignmentRecLong() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testAddAlignment() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSaveAlignment() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testDeleteAlignment() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testAddAlignmentProp() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSaveAlignmentProp() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testDeleteAlignmentProp() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testAddAlignmentArtifact() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSaveAlignmentArtifact() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testDeleteAlignmentArtifact() {
+//        fail("Not yet implemented");
+//    }
+
+}
