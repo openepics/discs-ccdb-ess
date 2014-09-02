993878540e207540b28b3740049d8ee40e6981e4
Miha Vitorovic <miha.vitorovic@cosylab.com>
2014-09-02 10:58:31 +0200

    CCDB-64: Device Types Service Layer
    Task-Url: https://ess-ics.atlassian.net/browse/CCDB-64
    
    Converted installation EJB to DAO.

--- conf-core/src/main/java/org/openepics/discs/conf/ejb/InstallationEJB.java
+++ conf-core/src/main/java/org/openepics/discs/conf/ejb/InstallationEJB.java
@@ -1,112 +1,35 @@
 package org.openepics.discs.conf.ejb;
 
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaQuery;
 
-import org.openepics.discs.conf.auditlog.Audit;
-import org.openepics.discs.conf.ent.EntityTypeOperation;
 import org.openepics.discs.conf.ent.InstallationArtifact;
 import org.openepics.discs.conf.ent.InstallationRecord;
-import org.openepics.discs.conf.security.Authorized;
-import org.openepics.discs.conf.util.CRUDOperation;
 
 /**
  *
  * @author vuppala
  * @author Miroslav Pavleski <miroslav.pavleski@cosylab.com>
- * @author Miha Vitorovič <miha.vitorovic@cosylab.com>
  */
-@Stateless public class InstallationEJB {
-    private static final Logger logger = Logger.getLogger(InstallationEJB.class.getCanonicalName());
-    @PersistenceContext private EntityManager em;
-    @Inject private ConfigurationEntityUtility entityUtility;
+@Stateless
+public class InstallationEJB extends DAO<InstallationRecord> {
 
-    // ----- Installation Records
+    @Override
+    protected void defineEntity() {
+        defineEntityClass(InstallationRecord.class);
 
-    public List<InstallationRecord> findInstallationRec() {
-        final CriteriaQuery<InstallationRecord> cq = em.getCriteriaBuilder().createQuery(InstallationRecord.class);
-        cq.from(InstallationRecord.class);
+        defineParentChildInterface(InstallationArtifact.class, new ParentChildInterface<InstallationRecord, InstallationArtifact>() {
 
-        final List<InstallationRecord> installationRecs = em.createQuery(cq).getResultList();
-        logger.log(Level.FINE, "Number of installation records: {0}", installationRecs.size());
+            @Override
+            public List<InstallationArtifact> getChildCollection(InstallationRecord iRecord) {
+                return iRecord.getInstallationArtifactList();
+            }
 
-        return installationRecs;
-    }
-
-    @CRUDOperation(operation=EntityTypeOperation.CREATE)
-    @Audit
-    @Authorized
-    public void addIRecord(InstallationRecord irec) {
-        entityUtility.setModified(irec);
-        em.persist(irec);
-    }
-
-    @CRUDOperation(operation=EntityTypeOperation.UPDATE)
-    @Audit
-    @Authorized
-    public void saveIRecord(InstallationRecord irec) {
-        entityUtility.setModified(irec);
-        em.merge(irec);
-    }
-
-    /** Deletes the installation record.
-     * @param irec - the installation record to delete.
-     */
-    @CRUDOperation(operation=EntityTypeOperation.DELETE)
-    @Audit
-    @Authorized
-    public void deleteIRecord(InstallationRecord irec) {
-        final InstallationRecord mergedIRec = em.merge(irec);
-        em.remove(mergedIRec);
-    }
-
-    // ---------------- Installation Record Artifact ---------------------
-
-    @CRUDOperation(operation=EntityTypeOperation.UPDATE)
-    @Audit
-    @Authorized
-    public void addInstallationArtifact(InstallationArtifact artifact) {
-        final InstallationRecord parent = artifact.getInstallationRecord();
-
-        entityUtility.setModified(parent, artifact);
-
-        parent.getInstallationArtifactList().add(artifact);
-        em.merge(parent);
-    }
-
-    @CRUDOperation(operation=EntityTypeOperation.UPDATE)
-    @Audit
-    @Authorized
-    public void saveInstallationArtifact(InstallationArtifact artifact) {
-        final InstallationArtifact mergedArtifact = em.merge(artifact);
-
-        entityUtility.setModified(mergedArtifact.getInstallationRecord(), mergedArtifact);
-
-        logger.log(Level.FINE, "Installation Artifact: name: " + mergedArtifact.getName()
-                + ", description: " + mergedArtifact.getDescription()
-                + ", uri: " + mergedArtifact.getUri() + ", is_internal: " + mergedArtifact.isInternal());
-    }
-
-    /** Deletes the installation record artifact.
-     * @param artifact - the installation record artifact to delete.
-     */
-    @CRUDOperation(operation=EntityTypeOperation.UPDATE)
-    @Audit
-    @Authorized
-    public void deleteInstallationArtifact(InstallationArtifact artifact) {
-        final InstallationArtifact mergedArtifact = em.merge(artifact);
-        final InstallationRecord parent = mergedArtifact.getInstallationRecord();
-
-        entityUtility.setModified(parent);
-
-        parent.getInstallationArtifactList().remove(mergedArtifact);
-        em.remove(mergedArtifact);
+            @Override
+            public InstallationRecord getParentFromChild(InstallationArtifact child) {
+                return child.getInstallationRecord();
+            }
+        });
     }
 }
