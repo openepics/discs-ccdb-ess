3a79924fb36592882a001d3597508aaac1f36ac4
mpavleski <miroslav.pavleski@cosylab.com>
2014-09-05 11:33:18 +0200

    CCDB-55 Mergin master into integration_test to test before merging back

--- conf-core/src/test/java/org/openepics/discs/conf/ejb/ComptypeEJBIT.java
+++ conf-core/src/test/java/org/openepics/discs/conf/ejb/ComptypeEJBIT.java
@@ -1,11 +1,6 @@
 package org.openepics.discs.conf.ejb;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.List;
 import java.util.UUID;
