ccd4cede8ce44a8af465065cfb0772a9db119ece
Miha Vitorovič <miha.vitorovic@cosylab.com>
2016-01-21 08:29:38 +0100

    Fixes to properly generate jacoco exec file.

--- conf-core/scripts/integration-test.cmd
+++ conf-core/scripts/integration-test.cmd
@@ -1,3 +1,2 @@
-mvn clean package "-Djacoco.out.file=../target/unit-tests.exec" "-Djacoco.append=false" %*
-mvn jacoco:prepare-agent-integration failsafe:integration-test "-Dit.test=*EJBIT*" "-Djacoco.destFile=../target/ejb-it.exec" "-Djacoco.append=false"  %*
-mvn jacoco:prepare-agent-integration failsafe:integration-test "-Dit.test=*DataLoaderIT*" "-Djacoco.destFile=../target/dl-it.exec" "-Djacoco.append=false" %*
+mvn jacoco:prepare-agent-integration failsafe:integration-test -P jboss "-Dit.test=*EJBIT*" "-Djacoco.unit.skip=true" %*
+mvn jacoco:prepare-agent-integration failsafe:integration-test -P jboss "-Dit.test=*DataLoaderIT*" %*
