819581ef44d99fc312d0b937a92e8b62bfae64cb
Miha Vitorovič <miha.vitorovic@cosylab.com>
2016-01-28 10:36:05 +0100

    Fixed JavaDoc and some very basic source reformatting.

--- conf-core/src/main/java/org/openepics/discs/conf/ui/trees/Tree.java
+++ conf-core/src/main/java/org/openepics/discs/conf/ui/trees/Tree.java
@@ -12,12 +12,12 @@ import org.primefaces.model.TreeNode;
 /**
  * Most importantly Tree class has an "extrinsic" method to find tree node's children.
  * Theoretically this could be inside tree's root node, but then we couldn't use RootNodeWithChildren for all: powers, controls and connects trees.
- * 
+ *
  * Additionally Tree class contains global info about the tree: current selection of the nodes, current filter.
- * 
+ *
  * @author ilist
  *
- * @param <D>
+ * @param <D> the class of the elements used to build a tree of
  */
 public abstract class Tree<D> {
 	private List<FilteredTreeNode<D>> selectedNodes = new ArrayList<>();
@@ -29,34 +29,33 @@ public abstract class Tree<D> {
 	/**
 	 * Constructs the tree.
 	 * Tree knows how to generate new descendant nodes, so it needs slotEJB.
-	 * 
+	 *
 	 * @param slotEJB slotEJB
 	 */
 	public Tree(SlotEJB slotEJB) {
 		this.slotEJB = slotEJB;
 	}
-	
+
 	/**
 	 * The master method to create/get children of the tree.
-	 * 
+	 *
 	 * @param parent the parent node
 	 * @return children
 	 */
-	public abstract List<? extends BasicTreeNode<D>> getAllChildren(BasicTreeNode<D> parent);	
-	
-	
+	public abstract List<? extends BasicTreeNode<D>> getAllChildren(BasicTreeNode<D> parent);
+
 	/**
 	 * The root node of this tree.
-	 * 
+	 *
 	 * @param rootNode root node
 	 */
 	public void setRootNode(FilteredTreeNode<D> rootNode) {
 		this.rootNode = rootNode;
 	}
-	
+
 	/**
 	 * Returns the root node.
-	 * 
+	 *
 	 * @return the root node
 	 */
 	public FilteredTreeNode<D> getRootNode() {
@@ -67,29 +66,26 @@ public abstract class Tree<D> {
 	 * Returns currently selected nodes.
 	 * @return selected nodes
 	 */
-	public List<FilteredTreeNode<D>> getSelectedNodes()
-	{
+	public List<FilteredTreeNode<D>> getSelectedNodes()	{
 		return selectedNodes;
-	}	
-	
+	}
+
 	/**
 	 * Returns currently selected nodes as an array. Used for primefaces.
 	 * @return array of selected nodes
 	 */
-	public TreeNode[] getSelectedNodesArray()
-	{
+	public TreeNode[] getSelectedNodesArray() {
 		return selectedNodes.toArray(new TreeNode[selectedNodes.size()]);
 	}
-	
+
 	/**
 	 * Sets selected nodes from an array. Used for primefaces.
 	 * Unchecked typecast is needed.
-	 * 
+	 *
 	 * @param selectedNodes array of selected nodes
 	 */
 	@SuppressWarnings("unchecked")
-	public void setSelectedNodesArray(TreeNode[] selectedNodes)
-	{
+	public void setSelectedNodesArray(TreeNode[] selectedNodes)	{
 		this.selectedNodes.clear();
 		if (selectedNodes != null)
 			this.selectedNodes.addAll((List<FilteredTreeNode<D>>)(List<?>)Arrays.asList(selectedNodes));
@@ -98,13 +94,13 @@ public abstract class Tree<D> {
 	/**
 	 * Clears the selection globally.
 	 */
-    public void unselectAllTreeNodes() {        
+    public void unselectAllTreeNodes() {
     	for (final TreeNode node : selectedNodes) {
     		node.setSelected(false);
         }
         selectedNodes = new ArrayList<>();
     }
-	
+
 	/**
 	 * Gets current filter field. Which might not be applied. Used for textboxes.
 	 * @return filter text
@@ -120,7 +116,7 @@ public abstract class Tree<D> {
 	public void setFilter(String filter) {
 		this.filter = filter;
 	}
-	
+
 	/**
 	 * Returns currently applied filter.
 	 * @return the applied filter
@@ -128,20 +124,19 @@ public abstract class Tree<D> {
 	public String getAppliedFilter() {
 		return appliedFilter;
 	}
-	
+
 	/**
 	 * Applies the filter. AppliedFilter filed becomes filter field.
 	 */
 	public void applyFilter() {
 		this.appliedFilter = filter == null ? "" : filter.toUpperCase();
-		((FilteredTreeNode<D>)getRootNode()).cleanFilterCache();
-	//	print(0, getRootNode());
+		getRootNode().cleanFilterCache();
 	}
 
 	/**
 	 * Returns whether the node should be displayed when filtering or not.
 	 * Implementation depends on the data in the tree.
-	 * 
+	 *
 	 * @param node the node
 	 * @return should the node be displayed
 	 */
@@ -149,33 +144,33 @@ public abstract class Tree<D> {
 
     /**
      * Refreshes nodes which contain slot with the given IDs.
-     * 
+     *
      * @param ids set of IDs
      */
     public void refreshIds(HashSet<Long> ids) {
     	refreshIds(getRootNode(), ids);
     }
-    
-    
+
+
     /**
      * Traverses the tree and refreshes given IDs.
      * TODO possible optimizations:
      *   1. keep a map of lists of tree nodes id -> list treenode
-     *   2. getBufferedAllChildren also buffers / better if just skip unbuffered nodes 
-     * 
+     *   2. getBufferedAllChildren also buffers / better if just skip unbuffered nodes
+     *
      * @param node currently visited node
      * @param ids set of IDs
-     */    
+     */
     private void refreshIds(FilteredTreeNode<D> node, HashSet<Long> ids) {
-    	   if (ids.contains(((SlotView)node.getData()).getId())) {
-    		   node.refreshCache();
-    	   } else {
-    		   for (final FilteredTreeNode<D> child : node.getBufferedAllChildren()) {
-    			   refreshIds(child, ids);
-    		   }
-    	   }
+	   if (ids.contains(((SlotView)node.getData()).getId())) {
+		   node.refreshCache();
+	   } else {
+		   for (final FilteredTreeNode<D> child : node.getBufferedAllChildren()) {
+			   refreshIds(child, ids);
+		   }
+	   }
     }
-    
+
     /**
      * Prints the tree with level numbers. Used for the debugging purposes.
      * @param i the starting level
