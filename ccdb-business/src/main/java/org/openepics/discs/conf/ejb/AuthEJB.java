0e1ef35ad7842f040b9697ac7f4d204711fa6837
unknown <vuppala@VUPPALA-WIN7.intranet.nscl.msu.edu>
2016-04-12 15:10:47 -0400

    updated slot page. updated slot tree. added auth pages.

---
+++ ccdb-business/src/main/java/org/openepics/discs/conf/ejb/AuthEJB.java
@@ -0,0 +1,63 @@
+/*
+ * This software is Copyright by the Board of Trustees of Michigan
+ * State University (c) Copyright 2012.
+ *
+ * You may use this software under the terms of the GNU public license
+ *  (GPL). The terms of this license are described at:
+ *       http://www.gnu.org/licenses/gpl.txt
+ *
+ * Contact Information:
+ *   Facilitty for Rare Isotope Beam
+ *   Michigan State University
+ *   East Lansing, MI 48824-1321
+ *   http://frib.msu.edu
+ *
+ */
+package org.openepics.discs.conf.ejb;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.ejb.Stateless;
+import org.openepics.discs.conf.ent.Privilege;
+
+import org.openepics.discs.conf.ent.Rack;
+import org.openepics.discs.conf.ent.Role;
+import org.openepics.discs.conf.ent.User;
+import org.openepics.discs.conf.ent.UserRole;
+
+/**
+ *
+ * @author vuppala
+ *
+ */
+@Stateless
+public class AuthEJB extends DAO<Role> {    
+    private static final Logger logger = Logger.getLogger(AuthEJB.class.getName());
+    
+    @Override
+    protected Class<Role> getEntityClass() {
+        return Role.class;
+    }
+
+    /**
+     * All the tacks
+     * 
+     * @return a list of all {@link Rack}s ordered by name.
+     */
+    public List<Role> findAllRoles() {
+        return em.createNamedQuery("Role.findAll", Role.class).getResultList();
+    }
+    
+    public List<Privilege> findAllPrivileges() {
+        return em.createNamedQuery("Privilege.findAll", Privilege.class).getResultList();
+    }
+    
+    public List<User> findAllUsers() {
+        return em.createNamedQuery("User.findAll", User.class).getResultList();
+    }
+    
+    public List<UserRole> findAllUserRoles() {
+        return em.createNamedQuery("UserRole.findAll", UserRole.class).getResultList();
+    }
+}
---
+++ conf-core/src/main/java/org/openepics/discs/conf/ui/AuthView.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openepics.discs.conf.ui;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.openepics.discs.conf.ejb.AuthEJB;
+import org.openepics.discs.conf.ent.Privilege;
+import org.openepics.discs.conf.ent.Role;
+import org.openepics.discs.conf.ent.User;
+import org.openepics.discs.conf.ent.UserRole;
+
+/**
+ *
+ * @author vuppala
+ */
+@Named
+@ViewScoped
+public class AuthView implements Serializable {
+    public static class Permission implements Serializable {
+        public String role;
+        public String oper;
+        public String resource;
+        
+        public Permission() {
+            
+        }
+        
+        public Permission(String role, String oper, String resource) {
+            this.role = role;
+            this.oper = oper;
+            this.resource = resource;
+        }
+        
+        public static Permission toPermission(Privilege priv) {
+            return new Permission(priv.getRole().getRoleId(), priv.getOper().toString(), priv.getResource().toString());            
+        }
+
+        public String getRole() {
+            return role;
+        }
+
+        public String getOper() {
+            return oper;
+        }
+
+        public String getResource() {
+            return resource;
+        }
+        
+        
+    }
+    
+    @Inject private AuthEJB authEJB;
+    private static final Logger logger = Logger.getLogger(AuthView.class.getName());
+
+    private List<Role> roles;
+    private List<Privilege> privileges;
+    private List<Permission> permissions = new ArrayList<>();
+    private List<User> users;
+    private List<UserRole> userRoles;
+    
+    public AuthView() {
+    }
+
+    @PostConstruct
+    public void init() {
+        roles = authEJB.findAllRoles();
+        users = authEJB.findAllUsers();
+        userRoles = authEJB.findAllUserRoles();
+        privileges = authEJB.findAllPrivileges();
+        for (Privilege priv: privileges) {
+            permissions.add(Permission.toPermission(priv));
+        }
+        permissions.add(new Permission("FE_AreaManager", "MANAGE", "FrontEnd"));
+        permissions.add(new Permission("PS_Manager", "UPDATE", "PowerSupplies"));
+        logger.log(Level.FINE, "Found number of slots: {0}", users.size());       
+    }
+    
+    // --- getters/setters
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public List<Privilege> getPrivileges() {
+        return privileges;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+}
